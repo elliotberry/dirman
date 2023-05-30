a3c2f1971e8b000228c3306643c0ad1b6796b741
E Berry <elliot@daddy.cat>
2023-05-30 08:07:12 -0400

    great work

--- output-list-of-files-not-in-second-folder.js
+++ output-list-of-files-not-in-second-folder.js
@@ -1,17 +1,22 @@
-
 import crawlOneDir from './lib/crawl-one-folder.js';
-import compareTwoFolders from './lib/compare-two-folders.js';
 import findConflicts from './lib/find-conflicts-promise.js';
-import saveLog from './lib/save-log.js';
+import display from './lib/display-conflicts-in-console.js';
+const compareOptions = {
+  hash: true,
+  size: false,
+  name: false
+};
 
 async function crawlTwoDirs(dir1, dir2) {
-  let one = await crawlOneDir(dir1);
-  let two = await crawlOneDir(dir2);
-
-  let compared = await findConflicts(one, two);
-  await saveLog({compared, dir1, dir2}, './compared.json');
-  console.log(compared.isNotInTargetFolder.map(file => file.absolutePath).join('\n'));
+  let one = await crawlOneDir(dir1, compareOptions);
+  console.log(`done crawling ${dir1}`);
+  let two = await crawlOneDir(dir2, compareOptions);
+  console.log(two);
+  console.log(`done crawling ${dir2}`);
+  console.log(`comparing ${dir1} to ${dir2}`);
+  let compared = await findConflicts(one, two, compareOptions);
+  display(compared, dir1, dir2, compareOptions);
+  
 }
 crawlTwoDirs('/Volumes/fatboi/olde/art', '/Users/eberry/Desktop/working-files-to-backup/art');
-
-
+//crawlTwoDirs('test/folder1', 'test/folder2');
