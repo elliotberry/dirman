6a90c4ed73d17cdb4da7c682a8a85ca9cb318b42
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-07-06 18:23:47 -0400

    general cleanup.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import pluginImport from "eslint-plugin-import"
 import perfectionistNatural from "eslint-plugin-perfectionist/configs/recommended-natural"
 import prettierConfig from "eslint-plugin-prettier/recommended"
 import is from "eslint-plugin-simple-import-sort"
@@ -16,9 +15,6 @@ export default [
       sourceType: "module"
     },
     plugins: {
-      import: pluginImport,
-      pluginImport,
-
       "simple-import-sort": is
     },
 
@@ -26,8 +22,8 @@ export default [
       "no-console": "off",
       "no-unused-vars": "warn",
 
-      "simple-import-sort/exports": "warn",
-      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
       "unicorn/no-null": "warn",
       "unicorn/prefer-top-level-await": "off",
       "unicorn/prevent-abbreviations": "warn"
