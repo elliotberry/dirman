cc7515e3a5e1d3bbaa13cbd04e4678837374f492
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2024-10-23 16:26:33 -0400

    mostly screwing around with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
+import importPlugin from "eslint-plugin-import"
+
 import perfectionist from "eslint-plugin-perfectionist"
 import prettierConfig from "eslint-plugin-prettier/recommended"
 import is from "eslint-plugin-simple-import-sort"
+import sonarjs from "eslint-plugin-sonarjs"
 import eslintPluginUnicorn from "eslint-plugin-unicorn"
 
 export default [
   prettierConfig,
   eslintPluginUnicorn.configs["flat/recommended"],
+  sonarjs.configs.recommended,
+  importPlugin.flatConfigs.recommended,
 
   {
     files: ["**/*.js"],
